ffef99115e9ca9bc8a6ba25bdb9c4605b17088c6
KKS <kirill.kuliuckin2016@yandex.ru>
2022-03-19 18:22:02 +0300

    Interface works with selects

--- src/main/resources/static/script2.js
+++ src/main/resources/static/script2.js
@@ -1,8 +1,23 @@
 let studentID = 1
+let interaction = 1
 let dataElements=[]
 let dataElementPresentations = {}
 let table = null
 let dataElementTableRows = {}
+
+
+function nextInteraction()
+{
+    switch (interaction)
+    {
+        case 1:
+            addDataElementDirections()
+            break
+    }
+    interaction++
+    let nextInteractionButton = document.getElementById("next-interaction")
+    nextInteractionButton.hidden=true
+}
 
 function checkDataElementBorders()
 {
@@ -64,6 +79,14 @@ function checkDataElementBorders()
             {
                 alert(obj.message)
             }
+            if (obj.hasOwnProperty("interaction"))
+            {
+                if (obj.interaction === "1")
+                {
+                    let nextInteractionButton = document.getElementById("next-interaction")
+                    nextInteractionButton.hidden=false
+                }
+            }
         }
 
     }
@@ -74,11 +97,94 @@ function addDataElementDirections()
 {
     for (let el in dataElementTableRows)
     {
-        let select = document.createElement("select")
-        let option = document.createElement("option")
-        option.value="input"
-        option.text="Входные данные"
-        select.appendChild(option)
-        dataElementTableRows[el].appendChild(select)
+        let td = document.createElement("td")
+        td.appendChild(createSelectBlock({"input":"Входные данные","output":"Выходные данные","updatable":"Обновляемые данные"}, "answer/2", {"student":studentID,"elementName":el}, "elementDirection"))
+        //td.appendChild(createSelect({"input":"Входные данные","output":"Выходные данные","updatable":"Обновляемые данные"}))
+        dataElementTableRows[el].appendChild(td)
     }
+}
+
+function addDataElementPresentations()
+{
+    for (let key in dataElements)
+    {
+        let currElement = dataElements[key]["name"]
+        let xhr = new XMLHttpRequest()
+        xhr.open("GET","/answer/presentations?student="+studentID+"&elementName="+currElement)
+        xhr.responseType="json"
+        xhr.send()
+        xhr.onload = () => {
+            let obj = xhr.response
+            dataElementPresentations[currElement] = obj
+
+            let td = document.createElement("td")
+            td.appendChild(createSelect(obj))
+            dataElementTableRows[currElement].appendChild(td)
+        }
+    }
+}
+
+
+function createSelectBlock(options, url, paramDict, thisParamName)
+{
+    let block = document.createElement("div")
+    let select = createSelect(options)
+    let div = document.createElement("div")
+    div.classList.add("alert")
+
+    block.appendChild(select)
+    block.appendChild(div)
+
+    let paramString = ""
+    for (let key in paramDict)
+    {
+        paramString += key+"="+paramDict[key]+"&"
+    }
+    paramString += thisParamName+"="
+
+    select.onchange = (ev) => {
+        //console.log(ev.srcElement.value);
+        //console.log(url)
+
+        let xhr = new XMLHttpRequest()
+        xhr.open("POST", url+"?"+paramString+ev.srcElement.value)
+        xhr.responseType="json"
+        xhr.send()
+        console.log("Response")
+        xhr.onload = () => {
+            let obj = (xhr.response)
+            if (obj.correct === "true")
+            {
+                select.disabled=true
+                div.hidden=true
+            }
+            else
+            {
+                div.classList.add("alert-danger")
+                div.innerText = obj.message
+            }
+        }
+
+
+    }
+    return block
+}
+
+function createSelect(options)
+{
+    let select = document.createElement("select")
+    let optionSelect = document.createElement("option")
+    optionSelect.text = "..."
+    optionSelect.disabled = true
+    optionSelect.selected = true
+    select.appendChild(optionSelect)
+    for (let val in options)
+    {
+        let option = document.createElement("option")
+        option.value = val
+        option.text = options[val]
+        select.appendChild(option)
+    }
+
+    return select
 }
