856fff0a582fdc4f16d624262833a1c1eb84aca5
KKS <kirill.kuliuckin2016@yandex.ru>
2022-03-20 12:10:19 +0300

    Interactions 1-5 are ready

--- src/main/resources/static/script2.js
+++ src/main/resources/static/script2.js
@@ -5,6 +5,18 @@ let dataElementPresentations = {}
 let table = null
 let dataElementTableRows = {}
 
+let components = {}
+let componentTables = {}
+let componentsTableRows = {}
+
+let componentsWithTransferMethods = {}
+
+let componentParameters = {}
+let returnComponent = null
+
+document.onload = ()=>{
+    document.getElementById("next-interaction").hidden=true
+}
 
 function nextInteraction()
 {
@@ -12,6 +24,15 @@ function nextInteraction()
     {
         case 1:
             addDataElementDirections()
+            break
+        case 2:
+            addDataElementPresentations()
+            break
+        case 3:
+            addComponents()
+            break
+        case 4:
+            addComponentsDataTypes()
             break
     }
     interaction++
@@ -64,6 +85,12 @@ function checkDataElementBorders()
                 if (dataElements.length===0)
                 {
                     table = document.createElement("table")
+                    table.style.margin = "20px"
+                    table.style.padding = "20px"
+                    table.classList.add("table")
+                    table.classList.add("table-striped")
+                    let tbody = document.createElement("tbody")
+                    table.appendChild(tbody)
                     document.getElementsByTagName("body").item(0).appendChild(table)
                 }
                 dataElements.push(new Object({name:obj.name, mission:obj.mission}))
@@ -71,7 +98,7 @@ function checkDataElementBorders()
                 let td = document.createElement("td")
                 td.innerText = obj.mission
                 tr.appendChild(td)
-                table.appendChild(tr)
+                table.getElementsByTagName("tbody").item(0).appendChild(tr)
                 dataElementTableRows[obj.name]=tr
                 alert("Добавлен новый элемент")
             }
@@ -98,7 +125,7 @@ function addDataElementDirections()
     for (let el in dataElementTableRows)
     {
         let td = document.createElement("td")
-        td.appendChild(createSelectBlock({"input":"Входные данные","output":"Выходные данные","updatable":"Обновляемые данные"}, "answer/2", {"student":studentID,"elementName":el}, "elementDirection"))
+        td.appendChild(createSelectBlock({"input":"Входные данные","output":"Выходные данные","updatable":"Обновляемые данные"}, "answer/2", {"student":studentID,"elementName":el}, "elementDirection",2))
         //td.appendChild(createSelect({"input":"Входные данные","output":"Выходные данные","updatable":"Обновляемые данные"}))
         dataElementTableRows[el].appendChild(td)
     }
@@ -118,19 +145,91 @@ function addDataElementPresentations()
             dataElementPresentations[currElement] = obj
 
             let td = document.createElement("td")
-            td.appendChild(createSelect(obj))
+            td.appendChild(createSelectBlock(obj,"/answer/3",{"student":studentID,"elementName":currElement}, "elementPresentation",3))
             dataElementTableRows[currElement].appendChild(td)
         }
     }
 }
 
+function addComponents()
+{
+    let xhr = new XMLHttpRequest()
+    xhr.open("GET","/answer/components?student="+studentID)
+    xhr.responseType="json"
+    xhr.send()
+    xhr.onload = () => {
+        components = xhr.response
+        for (let key in components)
+        {
+            let td = document.createElement("td")
+            //td.innerText = components[key]
+            let componentsTable = document.createElement("table")
+            componentsTableRows[key] = {}
+            for (let compName in components[key])
+            {
+                let tr = document.createElement("tr")
+                let currTd = document.createElement("td")
+                componentsTable.appendChild(tr)
+                tr.appendChild(currTd)
+                currTd.innerText = (components[key].length === 1) ? " " : components[key][compName].mission
+                componentsTableRows[key][components[key][compName].name] = tr
+            }
+            td.appendChild(componentsTable)
+            dataElementTableRows[key].appendChild(td)
+            componentTables[key]=componentsTable
+        }
+        addComponentsTransferMethods()
+    }
+}
 
-function createSelectBlock(options, url, paramDict, thisParamName)
+function addComponentsTransferMethods()
+{
+    for (let dataElementName in components)
+    {
+        let currComponents = components[dataElementName]
+        for (let i in currComponents)
+        {
+            let componentName = currComponents[i].name
+            let options = {"read-only":"Входной параметр","write-only":"Выходной параметр","read-write":"Обновляемый параметр","return":"Возвращаемое значение"}
+            let params = {"student":studentID,"elementName":dataElementName,"componentName":componentName}
+            let block = createSelectBlock(options,"/answer/4",params,"transferMethod",4)
+            componentsTableRows[dataElementName][componentName].appendChild(block)
+        }
+    }
+}
+
+function addComponentsDataTypes()
+{
+    for (let dataElementName in componentParameters)
+    {
+        for (let componentName in componentParameters[dataElementName])
+        {
+            let tr = componentsTableRows[dataElementName][componentName]
+            let options = {"Type_Int": "int", "Type_PointerToInt" : "int *", "Type_Float" : "float", "Type_Char": "char"}
+            let block = createSelectBlock(options, "/answer/5", {'student':studentID,'parameterName':componentParameters[dataElementName][componentName]}, "datatype")
+            let td = document.createElement("td")
+            td.appendChild(block)
+            tr.appendChild(td)
+        }
+    }
+    if (returnComponent != null)
+    {
+        let tr = componentsTableRows[returnComponent.element][returnComponent.component]
+        let options = {"Type_Int": "int", "Type_PointerToInt" : "int *", "Type_Float" : "float", "Type_Char": "char"}
+        let block = createSelectBlock(options, "/answer/5", {'student':studentID,'parameterName':'return'}, "datatype")
+        let td = document.createElement("td")
+        td.appendChild(block)
+        tr.appendChild(td)
+    }
+}
+
+function createSelectBlock(options, url, paramDict, thisParamName, interactionNum)
 {
     let block = document.createElement("div")
     let select = createSelect(options)
     let div = document.createElement("div")
     div.classList.add("alert")
+    div.style.fontSize = "12px"
 
     block.appendChild(select)
     block.appendChild(div)
@@ -145,21 +244,29 @@ function createSelectBlock(options, url, paramDict, thisParamName)
     select.onchange = (ev) => {
         //console.log(ev.srcElement.value);
         //console.log(url)
-
+        div.classList.remove("alert-danger")
+        div.classList.add("alert-warning")
+        div.innerText="Подождите... Идет проверка..."
         let xhr = new XMLHttpRequest()
         xhr.open("POST", url+"?"+paramString+ev.srcElement.value)
         xhr.responseType="json"
         xhr.send()
         console.log("Response")
         xhr.onload = () => {
+            div.classList.remove("alert-warning")
+            div.classList.remove("alert-danger")
             let obj = (xhr.response)
             if (obj.correct === "true")
             {
+                div.classList.add("alert-success")
+                div.innerText="Верно"
                 select.disabled=true
-                div.hidden=true
+                setTimeout(()=>{div.hidden=true}, 500)
+                operationOfInteraction(interactionNum, paramDict, ev.srcElement.value)
             }
             else
             {
+                div.classList.remove("alert-warning")
                 div.classList.add("alert-danger")
                 div.innerText = obj.message
             }
@@ -173,6 +280,7 @@ function createSelectBlock(options, url, paramDict, thisParamName)
 function createSelect(options)
 {
     let select = document.createElement("select")
+    select.classList.add("form-select")
     let optionSelect = document.createElement("option")
     optionSelect.text = "..."
     optionSelect.disabled = true
@@ -187,4 +295,74 @@ function createSelect(options)
     }
 
     return select
+}
+
+
+let elementsWithDirections = []
+let elementsWithPresentations = []
+
+function operationOfInteraction(interactionNum, specificDict, selectedValue)
+{
+    switch (interactionNum)
+    {
+        case 2:
+            elementsWithDirections.push(specificDict.elementName)
+            if (elementsWithDirections.length === dataElements.length)
+            {
+                alert("Вы корректно выбрали направления для всех элементов данных. Для перехода к следующему позаданию нажмите \"Далее\"")
+                let nextInteractionButton = document.getElementById("next-interaction")
+                nextInteractionButton.hidden=false
+            }
+            break
+        case 3:
+            elementsWithPresentations.push(specificDict.elementName)
+            if (elementsWithPresentations.length === dataElements.length)
+            {
+                alert("Вы корректно выбрали представления для всех элементов данных. Для перехода к следующему позаданию нажмите \"Далее\"")
+                let nextInteractionButton = document.getElementById("next-interaction")
+                nextInteractionButton.hidden=false
+            }
+            break
+        case 4:
+            alert("Selected value:"+selectedValue)
+            if (!componentsWithTransferMethods.hasOwnProperty(specificDict.elementName))
+            {
+                componentsWithTransferMethods[specificDict.elementName]=[]
+            }
+            componentsWithTransferMethods[specificDict.elementName].push(specificDict.componentName)
+
+            if (selectedValue === "return")
+                returnComponent = {"element": specificDict.elementName, "component" : specificDict.componentName}
+            else
+            {
+                if (!componentParameters.hasOwnProperty(specificDict.elementName))
+                    componentParameters[specificDict.elementName]={}
+                if (components[specificDict.elementName].length>1)
+                    componentParameters[specificDict.elementName][specificDict.componentName] = specificDict.elementName + "_" + specificDict.componentName
+                else
+                    componentParameters[specificDict.elementName][specificDict.componentName] = specificDict.elementName
+            }
+
+
+            if (allComponentsHaveTransferMethods())
+            {
+                alert("Вы корректно выбрали способ передачи в функцию для всех компонентов. Для перехода к следующему позаданию нажмите \"Далее\"")
+                let nextInteractionButton = document.getElementById("next-interaction")
+                nextInteractionButton.hidden=false
+            }
+
+            break
+    }
+}
+
+function allComponentsHaveTransferMethods()
+{
+    for (let elementName in components)
+    {
+        if (!componentsWithTransferMethods.hasOwnProperty(elementName))
+            return false
+        if (componentsWithTransferMethods[elementName].length < components[elementName].length)
+            return false
+    }
+    return true
 }
